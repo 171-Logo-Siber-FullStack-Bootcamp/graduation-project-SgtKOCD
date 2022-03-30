7b0eec84f6e0d5eaf4895a794ed70c9c8e8106a9
Kağan Demiröz <e3_kaan@icloud.com>
2022-03-30 09:28:09 +0300

    up

--- server/server.js
+++ server/server.js
@@ -3,9 +3,19 @@ const Router = require('./router/router.js')
 const server = express() 
 const cors = require('cors'); 
 const logger = require('./logging');
- 
+const elasticClient = require('./elasticsearch/elasticClient');
 server.use(cors()); 
 server.use(express.json()) 
+
+elasticClient.ping({    
+    requestTimeout: 1000
+}, function (error) {
+    if (error) {
+        console.trace('Elasticsearch\'e erişilmiyor!');
+    } else {
+        console.log('Elasticsearch ayakta :)');
+    }
+});
 
 server.get('/', (req, res) => { //server get proccess
     res.send("Hello World") 
